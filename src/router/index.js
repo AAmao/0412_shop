607bdfee5c147243425085810854f670605a5fa0
maoruxia <1039295897@qq.com>
2018-08-28 21:18:54 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+/*
+路由器模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Msite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/msite',
+
+    },
+    {
+      path: '/msite',
+      component: Msite,
+      meta: {
+        showFooter: true  // 标识NavFooter组件是否显示
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true  // 标识NavFooter组件是否显示
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true  // 标识NavFooter组件是否显示
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true  // 标识NavFooter组件是否显示
+      }
+    }
+  ]
+})
